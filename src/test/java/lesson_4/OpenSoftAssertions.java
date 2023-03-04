b25289c51f2ec4861c40d1bbd161fcee23464cf3
Anton Zhirenok <zhirenok@list.ru>
2023-03-05 00:48:34 +0300

    add test openSoftAssertions

---
+++ src/test/java/lesson_4/OpenSoftAssertions.java
@@ -0,0 +1,39 @@
+package lesson_4;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OpenSoftAssertions {
+    @Test
+    public void openSoftAssertions(){
+
+        String str = "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
+
+        Configuration.browserSize = "1920x1080";
+
+        open("https://github.com/");
+        $("[aria-label=\"Search GitHub\"]").setValue("Selenide").pressEnter();
+        $(".repo-list .v-align-middle").click();
+        $("#wiki-tab").click();
+        $(".wiki-more-pages-link button").click();
+        $(byText("SoftAssertions")).shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+        $(".markdown-body").shouldHave(text(str));
+        $("#code-tab").click();
+    }
+}
