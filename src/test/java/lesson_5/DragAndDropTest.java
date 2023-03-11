abbc44f8fbfbcb55007d9a7314e99d11cc88f2b8
Anton Zhirenok <zhirenok@list.ru>
2023-03-11 22:20:53 +0300

    added lesson_5

---
+++ src/test/java/lesson_5/DragAndDropTest.java
@@ -0,0 +1,26 @@
+package lesson_5;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class DragAndDropTest {
+
+    @BeforeAll
+    static void before() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    public void dragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
