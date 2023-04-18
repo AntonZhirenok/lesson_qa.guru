7a51a93a33f0427d637a383b70d40f980b60cc6b
Anton Zhirenok <zhirenok@list.ru>
2023-04-18 16:06:01 +0300

    added tests unit12

---
+++ src/test/java/lesson_12/tests/AutomationPracticeForm.java
@@ -0,0 +1,44 @@
+package lesson_12.tests;
+
+
+import lesson_12.pages.RegistrationFormPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class AutomationPracticeForm {
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    TestData testData = new TestData();
+
+    @Test
+    @Tag("form")
+    void formTest(){
+        registrationFormPage.openPage()
+                .removeBanner()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserEmail(testData.email)
+                .setGender(testData.gender)
+                .setUserNumber(testData.phoneNumber)
+                .setDateOfBirth(testData.year, testData.month,testData.day)
+                .setSubjects(testData.subject)
+                .setHobbies(testData.hobby)
+                .setPictureLoading(testData.uploadFile)
+                .setAddress(testData.address)
+                .setState(testData.state)
+                .setCity(testData.cite)
+                .submit()
+                .verifyModal()
+                .verifyResult("Student Name",testData.firstName + " " + testData.lastName)
+                .verifyResult("Student Email",testData.email)
+                .verifyResult("Gender",testData.gender)
+                .verifyResult("Mobile",testData.phoneNumber)
+                .verifyResult("Date of Birth",testData.day + " " + testData.month + "," + testData.year)
+                .verifyResult("Subjects",testData.subject)
+                .verifyResult("Hobbies",testData.hobby)
+                .verifyResult("Picture","123.png")
+                .verifyResult("Address",testData.address)
+                .verifyResult("State and City",testData.state + " " + testData.cite);
+
+    }
+}
