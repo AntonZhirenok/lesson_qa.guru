aef84b722c6c628f5d6db9ca9d23364618468b6d
Anton Zhirenok <zhirenok@list.ru>
2023-04-18 18:44:22 +0300

    clean test

--- src/test/java/lesson_12/tests/AutomationPracticeForm.java
+++ src/test/java/lesson_12/tests/AutomationPracticeForm.java
@@ -19,45 +19,6 @@ public class AutomationPracticeForm {
     @Test
     @Tag("form")
     void formTest(){
-        Configuration.browserSize = "1920x1080";
-
-        open("https://demoqa.com/automation-practice-form");
-
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-
-        $("#firstName").setValue("Zhirenok");
-        $("#lastName").setValue("Anton");
-        $("#userEmail").setValue("zhirenok@list.ru");
-        $(byText("Other")).click();
-        $("#userNumber").setValue("8962880222");
-        $(".react-datepicker-wrapper").click();
-        $(".react-datepicker__month-select").$(byText("May")).click();
-        $(".react-datepicker__year-select").$(byText("1997")).click();
-        $(".react-datepicker__day--013").click();
-        $("#subjectsInput").setValue("English").pressEnter();
-        $(byText("Sports")).click();
-        $("#hobbies-checkbox-3").parent().click();
-        $("#uploadPicture").uploadFromClasspath("lesson_3\\123.png");
-        $("#currentAddress").setValue("Lenina street 174");
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $(byText("Noida")).click();
-        $("#submit").click();
-
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".modal-body").shouldHave(text("Zhirenok Anton"));
-        $(".modal-body").shouldHave(text("zhirenok@list.ru"));
-        $(".modal-body").shouldHave(text("Other"));
-        $(".modal-body").shouldHave(text("8962880222"));
-        $(".modal-body").shouldHave(text("13 May,1997"));
-        $(".modal-body").shouldHave(text("English"));
-        $(".modal-body").shouldHave(text("Sports, Music"));
-        $(".modal-body").shouldHave(text("123.png"));
-        $(".modal-body").shouldHave(text("Lenina street 174"));
-        $(".modal-body").shouldHave(text("NCR Noida"));
-        $("#closeLargeModal").click();
 
     }
 }
