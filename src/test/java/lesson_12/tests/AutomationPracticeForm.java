ef5f1e74eca8d426f9d424aa6043028998d8405b
Anton Zhirenok <zhirenok@list.ru>
2023-04-18 18:35:10 +0300

    clear form test

--- src/test/java/lesson_12/tests/AutomationPracticeForm.java
+++ src/test/java/lesson_12/tests/AutomationPracticeForm.java
@@ -13,32 +13,6 @@ public class AutomationPracticeForm {
     @Test
     @Tag("form")
     void formTest(){
-        registrationFormPage.openPage()
-                .removeBanner()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setUserEmail(testData.email)
-                .setGender(testData.gender)
-                .setUserNumber(testData.phoneNumber)
-                .setDateOfBirth(testData.year, testData.month,testData.day)
-                .setSubjects(testData.subject)
-                .setHobbies(testData.hobby)
-                .setPictureLoading(testData.uploadFile)
-                .setAddress(testData.address)
-                .setState(testData.state)
-                .setCity(testData.cite)
-                .submit()
-                .verifyModal()
-                .verifyResult("Student Name",testData.firstName + " " + testData.lastName)
-                .verifyResult("Student Email",testData.email)
-                .verifyResult("Gender",testData.gender)
-                .verifyResult("Mobile",testData.phoneNumber)
-                .verifyResult("Date of Birth",testData.day + " " + testData.month + "," + testData.year)
-                .verifyResult("Subjects",testData.subject)
-                .verifyResult("Hobbies",testData.hobby)
-                .verifyResult("Picture","123.png")
-                .verifyResult("Address",testData.address)
-                .verifyResult("State and City",testData.state + " " + testData.cite);
 
     }
 }
