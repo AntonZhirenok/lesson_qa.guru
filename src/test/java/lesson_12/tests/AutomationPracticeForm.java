7e307667f2f7f3916742ad8d60915bb93717003d
Anton Zhirenok <zhirenok@list.ru>
2023-04-18 23:07:21 +0300

    added selenoid

--- src/test/java/lesson_12/tests/AutomationPracticeForm.java
+++ src/test/java/lesson_12/tests/AutomationPracticeForm.java
@@ -1,24 +1,47 @@
 package lesson_12.tests;
 
 
-import com.codeborne.selenide.Configuration;
 import lesson_12.pages.RegistrationFormPage;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-public class AutomationPracticeForm {
+public class AutomationPracticeForm extends TestBase {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     TestData testData = new TestData();
 
     @Test
-    @Tag("form")
+    @Tag("lesson12")
     void formTest(){
+        open("/automation-practice-form");
+        registrationFormPage
+                .removeBanner()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserEmail(testData.email)
+                .setGender(testData.gender)
+                .setUserNumber(testData.phoneNumber)
+                .setDateOfBirth(testData.year, testData.month,testData.day)
+                .setSubjects(testData.subject)
+                .setHobbies(testData.hobby)
+                .setPictureLoading(testData.uploadFile)
+                .setAddress(testData.address)
+                .setState(testData.state)
+                .setCity(testData.cite)
+                .submit()
+                .verifyModal()
+                .verifyResult("Student Name",testData.firstName + " " + testData.lastName)
+                .verifyResult("Student Email",testData.email)
+                .verifyResult("Gender",testData.gender)
+                .verifyResult("Mobile",testData.phoneNumber)
+                .verifyResult("Date of Birth",testData.day + " " + testData.month + "," + testData.year)
+                .verifyResult("Subjects",testData.subject)
+                .verifyResult("Hobbies",testData.hobby)
+                .verifyResult("Picture","123.png")
+                .verifyResult("Address",testData.address)
+                .verifyResult("State and City",testData.state + " " + testData.cite);
 
     }
 }
