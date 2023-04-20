890fb1278a4583dd602fe0515a65e6890997b1fd
Anton Zhirenok <zhirenok@list.ru>
2023-04-20 17:00:19 +0300

    added lesson13

---
+++ src/test/java/lesson_13/tests/AutomationPracticeForm.java
@@ -0,0 +1,45 @@
+package lesson_13.tests;
+
+import lesson_12.pages.RegistrationFormPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AutomationPracticeForm extends TestBase {
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    TestData testData = new TestData();
+
+    @Test
+    @Tag("lesson13")
+    void formTest(){
+        open("/automation-practice-form" );
+        registrationFormPage
+                .removeBanner()
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserEmail(testData.email)
+                .setGender(testData.gender)
+                .setUserNumber(testData.phoneNumber)
+                .setDateOfBirth(testData.year, testData.month,testData.day)
+                .setSubjects(testData.subject)
+                .setHobbies(testData.hobby)
+                .setPictureLoading(testData.uploadFile)
+                .setAddress(testData.address)
+                .setState(testData.state)
+                .setCity(testData.cite)
+                .submit()
+                .verifyModal()
+                .verifyResult("Student Name",testData.firstName + " " + testData.lastName)
+                .verifyResult("Student Email",testData.email)
+                .verifyResult("Gender",testData.gender)
+                .verifyResult("Mobile",testData.phoneNumber)
+                .verifyResult("Date of Birth",testData.day + " " + testData.month + "," + testData.year)
+                .verifyResult("Subjects",testData.subject)
+                .verifyResult("Hobbies",testData.hobby)
+                .verifyResult("Picture","123.png")
+                .verifyResult("Address",testData.address)
+                .verifyResult("State and City",testData.state + " " + testData.cite);
+    }
+}
